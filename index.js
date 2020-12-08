8d4dca37e767b469df826cba822fa8cb454ad826
royossai <royossai@yahoo.com>
2020-12-08 15:46:10 +0100

    day 7

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 const { log, readTxt } = require('./codes/reader')
+
+Array.prototype.unique = function(){
+    let uniqueArray = [];
+    for(let i = 0; i < this.length; i++)
+        if(uniqueArray.indexOf(this[i]) === -1)
+            uniqueArray.push(this[i]);
+
+    return uniqueArray;
+}
+
+const buildRules = buffer => {
+    let object = {};
+
+    for (let item of buffer){
+        let holder = item.match(/(?<holder>^.*)(?=( contain))/);
+        if (holder !== null) {
+            holder = holder.groups.holder;
+            item = item.replace(/^.*(?=( contain))/, '');
+            let allSubs = false;
+            let subsObj = [];
+            while (!allSubs) {
+                let subs = item.match(/(?<digit>\d) (?<desc>\w* \w*) (bags|bag)/);
+                if (subs !== null) {
+                    item = item.replace(/\d \w* \w* bags|bag/, '').replace(' ,', '');
+                    let temp = {name: subs.groups.desc, quantity: parseInt(subs.groups.digit)};
+                    subsObj.push(temp);
+                    allSubs = item === ' contain .';
+                } else allSubs = true;
+            }
+            object[holder] = subsObj;
+        }
+    }
+
+    return object;
+}
+
+const getBagsWithKid = (needle, rules) => {
+    needle = needle.replace(' bags', '');
+    let holders = [];
+
+    for (const bag of Object.keys(rules))
+        if (rules[bag].some(rule => rule.name === needle))
+            holders.push(bag);
+
+    for (const holder of holders)
+        holders = holders.concat(getBagsWithKid(holder, rules));
+
+    return holders.unique();
+}
+
+const getNumbOfBagsWithin = (needle, rules) => {
+    let bags = rules[needle];
+    let subBags = 0;
+
+    for(const item of bags){
+        let numbOfBagsWithin = getNumbOfBagsWithin(item.name + ' bags', rules);
+        subBags += numbOfBagsWithin === 0 ? item.quantity: item.quantity * numbOfBagsWithin;
+        console.log(item, numbOfBagsWithin, subBags);
+    }
+
+    return subBags;
+}
+
+const day7_1 =  async () => {
+    let buffer = await readTxt('puzzle7.txt');
+    let rules = buildRules(buffer);
+    let gold = 'shiny gold bags';
+    let numbers = getBagsWithKid(gold, rules);
+    log(rules[gold]);
+    let numbOfBagsWithin = getNumbOfBagsWithin(gold, rules);
+    log(74, numbers.length);
+    log(75, numbOfBagsWithin);
+}
+
+day7_1();
